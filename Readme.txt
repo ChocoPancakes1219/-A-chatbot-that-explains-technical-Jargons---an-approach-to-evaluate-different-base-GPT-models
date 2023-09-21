c354088a174d275f5bb6553bfbb0df3e261aba95
ChocoPancakes1219 <81368296+ChocoPancakes1219@users.noreply.github.com>
2023-09-22 03:09:46 +0800

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+To run any of the chatbot, run the .py file named after the chatbot model (text-ada-001.py, text-babbage-001.py, text-curie-001.py, text-davinci-003.py)
+
+After the .py is running, open the domain that appears on the console in any web browser (default to http://127.0.0.1:5000 using chrome)
+
+* Keep in mind that the credits of the OpenAI API will expire on June 2023, attempting to run the .py file after that will result in error.
+* If wanted to access the language models after June 2023, please repolace the API key (line 26 in each .py file)
---
+++ text-curie-001.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# Flask interface template and javascript retrieved from https://github.com/dongido001/flask_chatbot
+from flask import Flask, request, jsonify, render_template
+import os
+import openai
+import requests
+import json
+import time
+
+app = Flask(__name__)
+Setting = "The following is a text conversation of a user talking to a bot. The bot can assist user with academic related questions, such as: explaining technical Jargons."
+
+
+class Timer:
+    def start(self):
+        self.start_time = time.time()
+
+    def current_time(self):
+        return time.time() - self.start_time
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+openai.api_key = "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"
+def Complete_text(input_text):
+    response = openai.Completion.create(
+
+        model="text-curie-001",
+        prompt=input_text,
+        max_tokens=200,
+        top_p=1,
+        frequency_penalty= 0,
+        presence_penalty= 0.6,
+        temperature=0.9,
+
+    )
+    return response.choices[0].text.strip()
+
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form['message']
+    print('Sending message: {}'.format(message))
+    timer = Timer()
+    timer.start()
+    fulfillment_text = Complete_text(Setting+"\n"+"User: "+message+"\nBot:")
+    print(timer.current_time())
+    print('Response: {}'.format(fulfillment_text))
+    response_text = {"message": fulfillment_text}
+
+    return jsonify(response_text)
+
+
+# run Flask app
+if __name__ == "__main__":
+    app.run()
